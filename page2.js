c8e5b008c7e2edd2d1c54c865be77aab9e871409
Shantanu Nisarg Vyas <89649293+Shantanu-Nisarg-Vyas@users.noreply.github.com>
2022-02-11 16:04:43 +0530

    Add files via upload

---
+++ page2.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB8WkRQTXHE6mgwfIfPOi9_SnQmVFju9s4",
+    authDomain: "letschatwebapp-f340b.firebaseapp.com",
+    databaseURL: "https://letschatwebapp-f340b-default-rtdb.firebaseio.com",
+    projectId: "letschatwebapp-f340b",
+    storageBucket: "letschatwebapp-f340b.appspot.com",
+    messagingSenderId: "561831737410",
+    appId: "1:561831737410:web:74c01ccd7009f83fe595db",
+    measurementId: "G-ZWS5SFMJ1C"
+};
+
+firebase.initializeApp(firebaseConfig);
